17bfdc70c1b49811d04b1223a863e870db89db37
李思远 <lisiyuan>
2022-05-13 11:45:53 +0800

    no message

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -4,6 +4,6 @@ module.exports = core;
 const utils = require('utils');
 function core() {
     // TODO
-    console.log('packages core111');
+    console.log('packages core111111');
     utils();
 }
--- packages/utils/lib/utils.js
+++ packages/utils/lib/utils.js
@@ -4,5 +4,5 @@ module.exports = utils;
 
 function utils() {
     // TODO
-    console.log('packages utils11');
+    console.log('packages utils11111');
 }
