6e0d0adb01c09fdbce496281c2d50201a20a38e8
李思远 <lisiyuan>
2022-05-16 11:46:04 +0800

    feat：发布

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -1,1 +1,9 @@
-#! /usr/bin/env nodeconsole.log('这是一个脚手架')
+#! /usr/bin/env node
+console.log('这是一个脚手架')
+try {
+    require('../lib/core.js').run(process.argv.slice(2, process.argv.length));
+    console.log('111')
+} catch (error) {
+    require('../lib/core.js')
+    console.log('222')
+}
